6d1aad21ad2b15bc90aa5f47e6e19026f2df73e4
Phil Scott <phil.scott@gmail.com>
2022-05-05 23:35:25 -0400

    Adding blazor server demo

---
+++ tests/BlazorServerTestApp/MonorailCss.cs
@@ -0,0 +1,8 @@
+global using static BlazorServerTestApp.MonorailCss;
+
+namespace BlazorServerTestApp;
+
+internal partial class MonorailCss
+{
+    public static string CssClass(string input) => input;
+}
---
+++ tests/BlazorServerTestApp/Program.cs
@@ -0,0 +1,43 @@
+using BlazorServerTestApp.Data;
+using Microsoft.Extensions.Caching.Memory;
+using MonorailCss;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddMemoryCache();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.MapGet("/styles/style.css", (IMemoryCache cache) =>
+{
+    var style = cache.GetOrCreate("monorail-style", _ =>
+    {
+        var framework = new CssFramework();
+        return framework.Process(Values());
+    });
+
+    return Results.Text(style, "text/css");
+});
+
+app.Run();
